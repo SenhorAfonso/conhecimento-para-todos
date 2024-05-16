3d06e7f5ef8549a71037e9ce3ee0fff39fae3f53
Herick <joao.gabriel@flipcrm.com.br>
2024-05-16 18:13:52 -0300

    Criação inicial de como será a loginPage/registerPage

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,29 @@
-import server from './server';
+import express from 'express';
+import path from 'path';
 
-class App {
+const app = express();
+const port = process.env.PORT || 3001;
 
-  constructor() {
-    this.start();
-  }
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-  private start() {
-    // connect to database
-    server.listen(3000, () => {
-      console.log('Server is listening at 3000 port');
-    });
-  }
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
 
-}
+app.get('/', (req, res) => {
+  res.render('login');
+});
 
-new App();
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  res.send(`Login realizado com sucesso! Usuário: ${username}`);
+});
+
+app.post('/register', (req, res) => {
+  const { newUsername, newPassword } = req.body;
+  res.send(`Cadastro realizado com sucesso! Novo usuário: ${newUsername}`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
