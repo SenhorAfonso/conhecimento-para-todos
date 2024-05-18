74d90cfbe9fdd0f98c06e858f7fd3af189cc7edb
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-18 14:42:26 -0300

    :bug: fixed(home/routes): fix courses routing

--- src/routes/homeRoutes.ts
+++ src/routes/homeRoutes.ts
@@ -4,6 +4,8 @@ import HomeController from '../controllers/homeController';
 const homeRouter = Router();
 
 homeRouter.get('/', HomeController.home);
+homeRouter.get('/login', HomeController.login);
 homeRouter.get('/search', HomeController.search);
+homeRouter.get('/watch', HomeController.watching);
 
 export default homeRouter;
