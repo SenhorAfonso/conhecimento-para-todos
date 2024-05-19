9e6a90657ad5e3c159baa6d393b1aabbd3a98818
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-18 22:12:42 -0300

    :recycle: refactor(users/schema): change user schemas fields

--- src/schema/users/user.schema.ts
+++ src/schema/users/user.schema.ts
@@ -1,17 +1,17 @@
 import { Schema, model, Document } from 'mongoose';
 
 const userSchema = new Schema({
-  name:       String,
-  birth:      String,
-  email:      String,
-  password:   String
+  fullName: String,
+  username: String,
+  email: String,
+  password: String
 }, { timestamps: true });
 
 export interface UserDocument extends Document {
-  name?:       string | null,
-  birth?:      string | null,
-  email?:      string | null,
-  password?:   string | null
+  name?: string | null,
+  birth?: string | null,
+  email?: string | null,
+  password?: string | null
 }
 
 export default model('User', userSchema);
