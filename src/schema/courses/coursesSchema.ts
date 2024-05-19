d7829f0d2f67a373ce8a8a1ed99d556e746c0f26
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-18 22:12:00 -0300

    :sparkles: feat(schema): add courses schema

--- src/schema/courses/coursesSchema.ts
+++ src/schema/courses/coursesSchema.ts
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
 
 const coursesSchema = new mongoose.Schema({
-  creator: { type: String },
+  instructor: { type: mongoose.Types.ObjectId, ref: 'instructorSchema' },
   topic: { type: String },
   title: { type: String },
   description: { type: String },
   src: { type: String },
+  id: { type: String },
+  tableOfContents: [{ title: { type: String }, desc: { type: String }, checked: { type: Boolean } }],
   avgRating: { type: Number },
   ratingQtd: { type: Number },
-  numberOfStudents: { type: String }
+  numberOfStudents: { type: String },
+  questions: { type: mongoose.Types.ObjectId, ref: 'questionsSchema' }
 });
 
 export default mongoose.model('coursesModel', coursesSchema);
