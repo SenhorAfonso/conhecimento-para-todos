0f9aeab1c88c9d9b0cdc682f5e4c4e626066fd98
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-18 22:13:21 -0300

    :wrench: config(serverConfig): add server config

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 const serverConfig = {
   SERVER_PORT: process.env.SERVER_PORT,
-  DATABASE_URI: process.env.DATABASE_URI
+  DATABASE_URI: process.env.DATABASE_URI,
+  JWT_SECRETE_KEY: process.env.JWT_SECRETE_KEY
 };
 
 export default serverConfig;
