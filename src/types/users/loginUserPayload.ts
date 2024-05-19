917c2bc779b2623d483d035eeee735928b574ff2
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-18 22:06:49 -0300

    :sparkles: feat(user): implement register and login functionality

---
+++ src/types/users/loginUserPayload.ts
@@ -0,0 +1,6 @@
+interface loginUserPayload {
+  email: string;
+  password: string;
+};
+
+export default loginUserPayload;
--- src/types/users/user.type.ts
+++
@@ -1,6 +0,0 @@
-export interface UserType{
-    name: string
-    birth: string
-    email: string
-    password: string
-}
